5f6c7f8db2fd0e8cfe1a150176ec4c3d8a136c13
Sophieke van Luenen <svanluenen@tudelft.nl>
2025-12-17 11:00:29 +0100

    Added the most_blacks redundant constraint

--- src/duplicates_solver/duplicates_solver.py
+++ src/duplicates_solver/duplicates_solver.py
@@ -9,6 +9,7 @@ from src.duplicates_solver.duplicates_constraints.duplicates_path_constraint imp
 from src.optimisation_rules.corner_check import corner_check
 from src.optimisation_rules.corner_close import corner_close
 from src.optimisation_rules.edge_pairs import edge_pairs
+from src.optimisation_rules.most_blacks import most_blacks
 from src.optimisation_rules.sandwiches import sandwiches
 
 
@@ -43,7 +44,8 @@ def duplicates_solver(n, board):
     # corner_close(is_black, m)
     # corner_check(board, is_black, duplicates, n, m)
     # sandwiches(board, is_black, duplicates, n, m)
-    edge_pairs(board, is_black, duplicates, n, m, has_duplicates=True)
+    # edge_pairs(board, is_black, duplicates, n, m, has_duplicates=True)
+    most_blacks(is_black, duplicates, n, m, has_duplicates=True)
 
     # Adjacency constraint
     duplicates_adjacent_constraint(n, is_black, m, duplicates)
--- src/optimisation_rules/most_blacks.py
+++ src/optimisation_rules/most_blacks.py
@@ -1,2 +1,24 @@
+import math
+import gurobipy as gp
+
 # Uses the fact that each row and column can have at most ceil(n/2) covered tiles
-def most_blacks(board, is_covered, duplicates, n, m, has_duplicates=False):
+def most_blacks(is_covered, duplicates, n, m, has_duplicates=False):
+    for i in range(n):
+        expr = gp.LinExpr()
+        for j in range(n):
+            if not has_duplicates:
+                expr += is_covered[i][j]
+            else:
+                if duplicates[i][j] != 0:
+                    expr += is_covered[i][j]
+        m.addConstr(expr <= math.ceil(n/2))
+
+    for j in range(n):
+        expr = gp.LinExpr()
+        for i in range(n):
+            if not has_duplicates:
+                expr += is_covered[i][j]
+            else:
+                if duplicates[i][j] != 0:
+                    expr += is_covered[i][j]
+        m.addConstr(expr <= math.ceil(n / 2))
